effe1c77bda5076edf836edc9a8beb40009b30ab
Robert Bronson <robert.j.bronson+cf@gmail.com>
2019-06-12 11:30:15 -0700

    initial commit frmo Spring init

---
+++ src/main/java/com/rjb888/week3App/songr/SongrApplication.java
@@ -0,0 +1,13 @@
+package com.rjb888.week3App.songr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongrApplication.class, args);
+	}
+
+}
