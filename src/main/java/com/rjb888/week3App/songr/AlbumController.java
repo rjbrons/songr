e8e56521ba62332c3a2f9c10ee1273319ef42c80
Robert Bronson <robert.j.bronson+cf@gmail.com>
2019-06-14 09:04:49 -0700

    finished up functionality to add to db

--- src/main/java/com/rjb888/week3App/songr/AlbumController.java
+++ src/main/java/com/rjb888/week3App/songr/AlbumController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class AlbumController{
@@ -16,6 +19,18 @@ public class AlbumController{
         Iterable<Album> albumSet = albumRepository.findAll();
         album.addAttribute("albumSet", albumSet);
         return "Album";
+    }
+
+    @GetMapping("/albums/add")
+    public String addAlbum(){
+        return "AddAlbum";
+    }
+
+    @PostMapping("/albums/add")
+    public RedirectView addAlbumToDB(@RequestParam String title, @RequestParam String artist, @RequestParam int songCount, @RequestParam int length, @RequestParam String imageUrl){
+        Album newAlbum = new Album(title, artist, songCount, length, imageUrl);
+        albumRepository.save(newAlbum);
+        return new RedirectView("/albums");
     }
 
 }
